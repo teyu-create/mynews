ae90307b1ff39c0b5b1e292f5dcc4bc27b99e687
yuria <aw0asgt@gmail.com>
2024-10-06 13:14:39 +0000

    PHP/Laravel09 課題３controller使用宣言追記

--- routes/practice.php
+++ routes/practice.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*「http://◯◯◯◯.jp/XXX というアクセスが来たときに、 
 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください*/
-
+use App\Http\Controllers\AAAController;
+//                       ^^^^^^^^^^^^^
+//                       コントローラ AAAController を使用するという宣言
 Route::get('XXX','AAAController@bbb');
