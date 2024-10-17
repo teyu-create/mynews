7c8eaf38192aec011e643415f771aba3ba686165
yuria <aw0asgt@gmail.com>
2024-10-17 11:04:01 +0000

    PHP/laravel13 課題３〜６

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,21 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function(){
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
-/*【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。
-web.phpを編集して、admin/profile/create にアクセスしたら ProfileController の add Action に、
-admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定してください*/
+/*【応用】 routes/web.php を編集して、 admin/profile/create に POSTメソッドでアクセスしたら 
+ProfileController の create Action に割り当てるように設定してください
+【応用】 routes/web.php を編集して、 admin/profile/edit に POSTメソッドでアクセスしたら 
+ProfileController の update Action に割り当てるように設定してください*/
+
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function(){
-    Route::get('profile/create','add')->middleware('auth');
-    Route::get('profile/edit','edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('profile/create','add')->name('profile.add');
+    Route::get('profile/edit','edit')->name('profile.edit');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::post('profile/edit','update')->name('profile.update');
 });
 Auth::routes();
 
